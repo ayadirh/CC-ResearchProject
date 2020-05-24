3127dc0272bbb5b5aafdea3c8a518cbc8335894b
ayadirh <hridayasagarsubedi@gmail.com>
2020-05-24 15:23:00 +0200

    fixing the build

--- hello_user.py
+++ hello_user.py
@@ -3,4 +3,4 @@ import json
 
 def hello_user_handler(event, context):
     user = event["queryStringParameters"]["user"]
-    return {"statusCode": 200, "body": json.dumps({"message": "Hello {user}!"})}
+    return {"statusCode": 200, "body": json.dumps({"message": f"Hello {user}!"})}
